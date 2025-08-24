0604156c9fdf22ddcc46b099b79df93a44c306fe
unknown <samuelgalindop@gmail.com>
2025-08-24 13:09:21 +0200

    Update V25.08.24.1309

--- src/app/[locale]/maps/page.tsx
+++ src/app/[locale]/maps/page.tsx
@@ -39,10 +39,11 @@ export default function MapsPage() {
                                         />
                                         <div className="absolute inset-0 bg-black/60 opacity-0 group-hover:opacity-100 transition-opacity flex items-center justify-center text-white px-4 text-base text-center rounded-2xl">
                                             {description}
+                                            <div className="absolute bottom-0 left-0 right-0 bg-black/50 text-white text-lg font-medium py-2 rounded-b-2xl">
+                                                {name}
+                                            </div>
                                         </div>
-                                        <div className="absolute bottom-0 left-0 right-0 bg-black/30 text-white text-lg font-medium py-2 rounded-b-2xl">
-                                            {name}
-                                        </div>
+                                        
                                     </div>
                                 </Link>
                             </Reveal>
